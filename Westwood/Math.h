8a5f0a7d24d71b9a92af6568e09ba5772403479b
Kasper Esbjörnsson <kasper.esbjornsson@gmail.com>
2018-06-27 21:15:33 +0200

    Added random in range and chance to math

--- Westwood/Math.h
+++ Westwood/Math.h
@@ -1,10 +1,35 @@
 #pragma once
 #include <SFML\System\Vector2.hpp>
+#include <random>
+#include <assert.h>
 
 namespace Math
 {
 	static float GetLenght2(const sf::Vector2f& a_vector)
 	{
 		return a_vector.x * a_vector.x + a_vector.y * a_vector.y;
+	}
+
+	static short RandomInRange(short a_min, short a_max)
+	{
+		static std::random_device randomDevice;
+		static std::mt19937 seedGenerator(randomDevice());
+
+		std::uniform_int_distribution<short> distributor(a_min, a_max);
+		short randomNumber = distributor(seedGenerator);
+
+		return randomNumber;
+	}
+
+	static bool Chance(short a_percentageChance)
+	{
+		short percentage = RandomInRange(0, 100);
+
+		if (a_percentageChance <= percentage)
+		{
+			return true;
+		}
+
+		return false;
 	}
 }
